be340612d8c8a48627ec86cbcec6f642a5a468c4
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-09-16 21:30:43 +0800

    feat: stats-ui panel

---
+++ Assets/Scripts/StatsUIPanel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manages all the UI updates in the stats panel.
+/// </summary>
+public class StatsUIPanel : MonoBehaviour
+{
+    public TMP_Text timeText;
+    public TMP_Text balanceText;
+
+    /// <summary>
+    /// Sets the time displayed in the stats panel.
+    /// </summary>
+    /// <param name="timeString">The new time string.</param>
+    public void UpdateTime(string timeString)
+    {
+        timeText.text = timeString;
+    }
+
+    /// <summary>
+    /// Sets the wallet balance displayed in the stats panel.
+    /// </summary>
+    /// <param name="balance">The wallet balance.</param>
+    public void UpdateBalance(int balance)
+    {
+        balanceText.text = "$" + balance.ToString();
+    }
+}
